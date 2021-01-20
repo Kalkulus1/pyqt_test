3ea6cf344862dbdef7cd474277b0b3d76bd7a45c
Mumuni Mohammed <mumunim@gmail.com>
2021-01-20 22:01:08 +0000

    Changed code to a class. Added a few touches.

--- example.py
+++ example.py
@@ -2,15 +2,33 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Kalkulus First Window")
+        self.initUI()
+        
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("My first label")
+        self.label.move(50,50)
+        
+        # Buttons
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText('Click Me')
+        self.b1.clicked.connect(self.clicked)
+        
+    def clicked(self):
+        self.label.setText("You pressed the button")
+        self.update()
+    
+    def update(self):
+        self.label.adjustSize()
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Kalkulus First Window")
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("My first label")
-    label.move(50,50)
+    win = MyWindow()
     
     win.show()
     sys.exit(app.exec_())
